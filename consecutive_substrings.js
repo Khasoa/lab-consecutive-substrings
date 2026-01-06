22d6e14a4515b71bf78ad9099184b67886575ba7
Lydia Khasoa <khasoalydia@gmail.com>
2026-01-06 11:29:20 +0300

    complete consecutive substrings

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -1,5 +1,16 @@
 function consecutiveSubstrings(string) {
-  // type your code here
+  // Array to store all substrings
+  const result = [];
+
+  //Outer loop controls the starting index
+  for (let i = 0; i < string.length; i++) {
+    //Inner loop controls the ending index
+    for (let j = i; j < string.length; j++) {
+      //Extract consecutive substring from index i to j
+      result.push(string.slice(i, j + 1));
+    }
+  }
+  return result;
 }
 
 if (require.main === module) {
